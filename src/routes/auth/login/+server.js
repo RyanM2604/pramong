42a038634624c94abc3c62bbaac2c952461591bf
RyanM2604 <darkfire2604@gmail.com>
2024-01-20 23:36:58 +0700

    bug fixing

--- src/routes/auth/login/+server.js
+++ src/routes/auth/login/+server.js
@@ -1,6 +1,6 @@
 import { json } from "@sveltejs/kit";
 import { createClient } from "@supabase/supabase-js";
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "virtual:$env/static/public";
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$lib/env";
 
 const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 
--- src/routes/auth/register/+server.js
+++ src/routes/auth/register/+server.js
@@ -1,7 +1,7 @@
 import { json } from "@sveltejs/kit";
 import { error } from "@sveltejs/kit";
 import { createClient } from "@supabase/supabase-js";
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$lib/env";
 
 const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 
